81bb3838fe96835166e0330a6fb2d48f3b426453
oomwoo <camera@ovsy.com>
2016-05-09 22:42:57 -0700

    Changed license to GNU LGPL v3

--- serial_test.py
+++ serial_test.py
@@ -7,8 +7,8 @@
 # Copyright (C) 2016 oomwoo.com
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3.0 as
-# published by the Free Software Foundation.
+# it under the terms of the GNU Lesser General Public License version 3.0
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
