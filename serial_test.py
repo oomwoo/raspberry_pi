f9ee04c80b14aa51a0dcfa66506e5c7ac4cc2255
Oom Woo <camera@ovsy.com>
2016-05-15 23:20:18 +0000

    Does not work with latest NOOBS 9.1.1!

--- serial_test.py
+++ serial_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Connect Raspberry Pi to VEX Cortex using bi-directional UART serial link
 # and test transmission
 # This code runs on Raspberry Pi (tested with Raspberry Pi 2 model B)
@@ -17,13 +18,18 @@
 
 import serial
 
+
+print "Warning: does not work with NOOBS 9.1.1, works with 9.1.0"
+
 port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+# For Raspberry Pi 3, use /dev/ttyAMA0
+# For Raspberry Pi 3, use /dev/ttyS0
 
 cmd = "s60\n"
 
 while True:
     port.write(cmd)
-    print cmd
+    print "I sent " + cmd
     rcv = port.readline()
 #    rcv = port.read(10)
     print "You sent: ", repr(rcv)
