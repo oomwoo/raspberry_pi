a4e2b8e1d8004b7410035b0f588db6c353cbaa9c
Oom Woo <camera@ovsy.com>
2016-09-01 21:28:58 -0700

    Cleaning up

--- test/neon_camera_test.py
+++ test/neon_camera_test.py
@@ -16,81 +16,72 @@
 Train robot to drive autonomously. Use a small convolutional neural network.
 """
 
+import os
+import time
 import picamera
 import picamera.array
 import numpy as np
 from PIL import Image
-
-fps = 90
-w = 320
-h = 240
-l = 64
-W = 64 # CNN input image size
-H = 64
-param_file_name = "trained_bot_model.prm"
-nclass = 4
-
-camera = picamera.PiCamera()
-camera.resolution = (w, h)
-camera.framerate = fps
-camera.exposure_mode = 'fixedfps'
-
-# Capture camera picture into numpy array
-stream = picamera.array.PiRGBArray(camera)
-camera.capture(stream, 'rgb', use_video_port=True)
-print(stream.array.shape)
-camera.close()
-
-# Resize image
-image = Image.fromarray(stream.array)
-size = H, W
-image = image.resize(size, Image.ANTIALIAS)
-image = np.asarray(image, dtype=np.float32)
-
-# run neural network
-
-# parse the command line arguments
-from neon.util.argparser import NeonArgparser
-parser = NeonArgparser(__doc__)
-args = parser.parse_args()
-args.batch_size = 1
-
-# Set up backend for inference
 from neon.backends import gen_backend
-be = gen_backend(backend='cpu', batch_size=1)
-
-from neon.layers import Affine, Conv, Pooling, GeneralizedCost
+from neon.layers import Affine, Conv, Pooling
 from neon.models import Model
 from neon.transforms import Rectlin, Softmax
 from neon.initializers import Uniform
 from neon.data.dataiterator import ArrayIterator
 
-init_uni = Uniform(low=-0.1, high=0.1)
 
-bn = True
+fps = 90
+w = 320
+h = 240
+W = 32  # CNN input image size
+H = W
+l = W
+home_dir = os.path.expanduser("~")
+param_file_name = home_dir + "/ubuntu/model/trained_bot_model_32x32.prm"
+classes = ["forward", "left", "right", "backward"]  # from ROBOT-C bot.c
+nclasses = len(classes)
+
+# Set up camera
+camera = picamera.PiCamera()
+camera.resolution = (w, h)
+camera.framerate = fps
+camera.exposure_mode = 'fixedfps'
+
+# Capture image
+stream = picamera.array.PiRGBArray(camera)
+camera.capture(stream, 'rgb', use_video_port=True)
+camera.close()
+print "Captured image " + stream.array.shape
+image = Image.fromarray(stream.array)
+# image.show()
+
+# Set up neural network
+be = gen_backend(backend='cpu', batch_size=1)    # NN backend
+init_uni = Uniform(low=-0.1, high=0.1)           # Unnecessary NN weight initialization
+bn = True                                        # enable NN batch normalization
 layers = [Conv((5, 5, 16), init=init_uni, activation=Rectlin(), batch_norm=bn),
           Pooling((2, 2)),
-          Conv((5, 5, 32), init=init_uni, activation=Rectlin(), batch_norm=bn),
+          Conv((3, 3, 32), init=init_uni, activation=Rectlin(), batch_norm=bn),
           Pooling((2, 2)),
-          Affine(nout=100, init=init_uni, activation=Rectlin(), batch_norm=bn),
-          Affine(nout=4, init=init_uni, activation=Softmax())]
-
-#model = Model(param_file_name)
+          Affine(nout=50, init=init_uni, activation=Rectlin(), batch_norm=bn),
+          Affine(nout=nclasses, init=init_uni, activation=Softmax())]
 model = Model(layers=layers)
 model.load_params(param_file_name, load_states=False)
 
-
-L = W*H*3
-x_new = np.zeros((1, L), dtype=np.float32)
-x_new[0] = image.reshape(1, L) # / 255
-
-inference_set = ArrayIterator(x_new, None, nclass=nclass, lshape=(3, H, W))
-
-# from ROBOT-C bot.c
-classes = ["forward", "left", "right", "backward"]
-
-import time
 start_time = time.time()
+
+# Convert image to sample
+size = H, W
+image = image.resize(size)  # , Image.ANTIALIAS)
+r, g, b = image.split()
+image = Image.merge("RGB", (b, g, r))
+image = np.asarray(image, dtype=np.float32)
+image = np.transpose(image, (2, 0, 1))
+x_new = image.reshape(1, L) - 127
+
+# Run neural network
+inference_set = ArrayIterator(x_new, None, nclass=nclasses, lshape=(3, H, W))
 out = model.get_outputs(inference_set)
+print "Recognized as " + class_names[out.argmax()]
+
 print "--- %s seconds --- " % (time.time() - start_time)
-print classes[out[0].argmax()]
